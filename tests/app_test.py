04eea4b2ac4f81afa64f3586bcabb2c180bf31b7
Shadman Siddiqui <shadman2606@gmail.com>
2020-10-30 17:39:20 -0400

    Add SQLAlchemy and search page

--- tests/app_test.py
+++ tests/app_test.py
@@ -78,3 +78,8 @@ def test_delete_message(client):
     rv = client.get('/delete/1')
     data = json.loads(rv.data)
     assert data["status"] == 1
+
+def test_search(client):
+    response = client.get("/search", content_type="html/text")
+    assert response.status_code == 200
+
